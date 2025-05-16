710b170e0343b94aebbe3a7b462db6556fbbd2b7
Akhmed <ahmedmalaciev4@gmail.com>
2025-05-16 21:42:35 +0300

    Сделал sidebar компонентом

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+  fetch('sidebar.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('sidebar-container').innerHTML = data;
+    })
+    .catch(error => console.error('Ошибка загрузки sidebar:', error));
+});
